affe95b3f0ef7e0ceeabdd393bca4d4c49c5ced6
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 02:28:03 -0800

    Renamed PosttestApplication to LotteryApplication

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/LotteryApplication.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/LotteryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PosttestApplication {
+public class LotteryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PosttestApplication.class, args);
+		SpringApplication.run(LotteryApplication.class, args);
 	}
 
 }

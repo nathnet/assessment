afd0499cbd07cd77e950003040869252858b2c6d
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 15:19:31 -0800

    Created request and response models for admin ticket creation

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/api/response/PostLotteryResponse.java
@@ -0,0 +1,16 @@
+package com.kbtg.bootcamp.posttest.api.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.NonNull;
+import lombok.Value;
+
+@Value
+@Builder(toBuilder = true)
+public class PostLotteryResponse {
+  
+  @NonNull
+  @JsonProperty("ticket")
+  String lotteryId;
+}

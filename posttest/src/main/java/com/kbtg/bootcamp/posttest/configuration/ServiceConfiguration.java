5068e1e9c3e1a5aa70d73ba859482e1f66fe0297
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 22:17:57 -0800

    Updated LotteryService and request

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/configuration/ServiceConfiguration.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/configuration/ServiceConfiguration.java
@@ -1,8 +1,21 @@
 package com.kbtg.bootcamp.posttest.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.kbtg.bootcamp.posttest.repository.LotteryRepository;
+import com.kbtg.bootcamp.posttest.service.LotteryService;
+
 
 @Configuration
 public class ServiceConfiguration {
-  
+
+  @Autowired
+  LotteryRepository lotteryRepository;
+
+  @Bean
+  public LotteryService lotteryService() {
+    return new LotteryService(lotteryRepository);
+  }
 }
--- posttest/src/main/java/com/kbtg/bootcamp/posttest/service/LotteryService.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/service/LotteryService.java
@@ -1,13 +1,41 @@
 package com.kbtg.bootcamp.posttest.service;
 
+import java.util.Optional;
+
 import com.kbtg.bootcamp.posttest.api.request.PostLotteryRequest;
 import com.kbtg.bootcamp.posttest.api.response.PostLotteryResponse;
+import com.kbtg.bootcamp.posttest.entity.Lottery;
+import com.kbtg.bootcamp.posttest.repository.LotteryRepository;
+
+import lombok.NonNull;
 
 public class LotteryService {
 
-  public LotteryService() {}
+  private final LotteryRepository lotteryRepository;
 
-  public PostLotteryResponse createOneLottery(PostLotteryRequest request) {
-    return PostLotteryResponse.builder().lotteryId("test").build();
+  public LotteryService(@NonNull final LotteryRepository lotteryRepository) {
+    this.lotteryRepository = lotteryRepository;
+  }
+
+  public PostLotteryResponse createOneLottery(final PostLotteryRequest request) {
+    String lotteryId = request.getLotteryId();
+    Optional<Lottery> optionalLottery = lotteryRepository.findById(lotteryId);
+
+    int totalAmount = request.getAmount();
+
+    if (optionalLottery.isPresent()) {
+        Lottery existingLottery = optionalLottery.get();
+        totalAmount += existingLottery.getAmountAvailable();
+    }
+
+    Lottery lottery = Lottery.builder()
+        .lotteryId(lotteryId)
+        .price(request.getPrice())
+        .amountAvailable(totalAmount)
+        .build();
+
+    lotteryRepository.save(lottery);
+
+    return PostLotteryResponse.builder().lotteryId(lotteryId).build();
   }
 }

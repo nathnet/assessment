047e2e128603b0eb78eb2d8697d62bb4b317b6c2
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 22:17:09 -0800

    Updated models

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/entity/Lottery.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/entity/Lottery.java
@@ -2,18 +2,22 @@ package com.kbtg.bootcamp.posttest.entity;
 
 import java.math.BigDecimal;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.Value;
+import lombok.Setter;
 
-@Value
+@Setter
+@Getter
 @Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "lottery")
 public class Lottery {

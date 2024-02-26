ec611c943d1a96f24a6ae3ed877b7e3bd4c34d9b
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 17:18:03 -0800

    Fixed package name not matching location

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/repository/LotteryRepository.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/repository/LotteryRepository.java
@@ -3,7 +3,7 @@ package com.kbtg.bootcamp.posttest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.kbtg.bootcamp.posttest.lottery.Lottery;
+import com.kbtg.bootcamp.posttest.entity.Lottery;
 
 @Repository
 public interface LotteryRepository extends JpaRepository<Lottery, String> {
--- posttest/src/main/java/com/kbtg/bootcamp/posttest/repository/UserLotteryRepository.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/repository/UserLotteryRepository.java
@@ -3,7 +3,7 @@ package com.kbtg.bootcamp.posttest.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.kbtg.bootcamp.posttest.user.UserLottery;
+import com.kbtg.bootcamp.posttest.entity.UserLottery;
 
 @Repository
 public interface UserLotteryRepository extends JpaRepository<UserLottery, Long> {

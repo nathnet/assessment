04b19febe0948869f69ba83afcf926bb3ccece94
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 22:17:57 -0800

    Updated LotteryService and request

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/api/request/PostLotteryRequest.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/api/request/PostLotteryRequest.java
@@ -5,18 +5,18 @@ import java.math.BigDecimal;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
-import lombok.NonNull;
 import lombok.Value;
 
 @Value
 @Builder(toBuilder = true)
 public class PostLotteryRequest {
   
-  @NonNull
+  @NotNull
   @NotBlank(message = "Ticket number cannot be empty.")
   @Size(min = 6, max = 6, message = "Ticker number must be exactly 6 digits.")
   @Pattern(regexp = "[0-9]{6}", message = "Ticket number should be from 0-9 for 6 digits. Example: 123456")

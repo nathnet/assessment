c5603190e8090957861d744c4c1407db1f48fc20
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 22:07:32 -0800

    Updated environment variables and database schema

--- posttest/database/init.sql
+++ posttest/database/init.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS user_lottery;
 
 CREATE TABLE lottery (
-  lottery_id char(6) PRIMARY KEY,
+  lottery_id varchar(6) PRIMARY KEY,
   price numeric(15, 2),
   amount_available int
 );
 
 CREATE TABLE users (
-  user_id char(10) PRIMARY KEY,
+  user_id varchar(10) PRIMARY KEY,
   name varchar(255),
   email varchar(255),
   password varchar(255)
@@ -17,8 +17,8 @@ CREATE TABLE users (
 
 CREATE TABLE user_lottery (
   transaction_id serial PRIMARY KEY,
-  lottery_id char(6) NOT NULL,
-  user_id char(10) NOT NULL,
+  lottery_id varchar(6) NOT NULL,
+  user_id varchar(10) NOT NULL,
   lottery_amount int,
   FOREIGN KEY (lottery_id) REFERENCES lottery (lottery_id),
   FOREIGN KEY (user_id) REFERENCES users (user_id)

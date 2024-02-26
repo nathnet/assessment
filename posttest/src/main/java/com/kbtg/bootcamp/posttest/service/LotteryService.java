fd3612ef3ec329e45a991b0e4aa8957c4a60317a
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-26 04:09:50 -0800

    Added functionalities for EXP03

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/service/LotteryService.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/service/LotteryService.java
@@ -9,8 +9,14 @@ import org.springframework.stereotype.Service;
 import com.kbtg.bootcamp.posttest.api.request.PostLotteryRequest;
 import com.kbtg.bootcamp.posttest.api.response.GetLotteryResponse;
 import com.kbtg.bootcamp.posttest.api.response.PostLotteryResponse;
+import com.kbtg.bootcamp.posttest.api.response.PostUserLotteryResponse;
 import com.kbtg.bootcamp.posttest.entity.Lottery;
+import com.kbtg.bootcamp.posttest.entity.User;
+import com.kbtg.bootcamp.posttest.entity.UserLottery;
+import com.kbtg.bootcamp.posttest.exception.BadRequestException;
 import com.kbtg.bootcamp.posttest.repository.LotteryRepository;
+import com.kbtg.bootcamp.posttest.repository.UserLotteryRepository;
+import com.kbtg.bootcamp.posttest.repository.UserRepository;
 
 import lombok.NonNull;
 
@@ -18,9 +24,15 @@ import lombok.NonNull;
 public class LotteryService {
 
   private final LotteryRepository lotteryRepository;
+  private final UserRepository userRepository;
+  private final UserLotteryRepository userLotteryRepository;
 
-  public LotteryService(@NonNull final LotteryRepository lotteryRepository) {
+  public LotteryService(@NonNull final LotteryRepository lotteryRepository,
+      @NonNull final UserRepository userRepository,
+      @NonNull final UserLotteryRepository userLotteryRepository) {
     this.lotteryRepository = lotteryRepository;
+    this.userRepository = userRepository;
+    this.userLotteryRepository = userLotteryRepository;
   }
 
   public GetLotteryResponse getAllLotteries() {
@@ -52,5 +64,24 @@ public class LotteryService {
     lotteryRepository.save(lottery);
 
     return PostLotteryResponse.builder().lotteryId(lotteryId).build();
+  }
+
+  public PostUserLotteryResponse purchaseOneLottery(String userId, String lotteryId) {
+    Optional<User> optionalUser = userRepository.findById(userId);
+    Optional<Lottery> optionalLottery = lotteryRepository.findById(lotteryId);
+
+    User user = optionalUser
+        .orElseThrow(() -> new BadRequestException(String.format("Your userID %s is incorrect", userId)));
+    Lottery lottery = optionalLottery
+        .orElseThrow(() -> new BadRequestException(String.format("The lottery you want to purchase with id %s does not exist", lotteryId)));
+
+    UserLottery userLottery = UserLottery.builder()
+        .user(user)
+        .lottery(lottery)
+        .lotteryAmount(1) // magic!
+        .build();
+    UserLottery savedUserLottery = userLotteryRepository.save(userLottery);
+
+    return PostUserLotteryResponse.builder().transactionId(savedUserLottery.getTransactionId()).build();
   }
 }

a28177dee70092965b9befde46883d3a86805a74
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-26 05:36:42 -0800

    Refactored BadRequestException to NotFoundException

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionHandler.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionHandler.java
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-  @ExceptionHandler({ BadRequestException.class })
-  public ResponseEntity<ApiErrorResponse> BadRequestException(BadRequestException e) {
-    ApiErrorResponse errorResponse = new ApiErrorResponse(e.getMessage(), HttpStatus.BAD_REQUEST, ZonedDateTime.now());
-    return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+  @ExceptionHandler({ NotFoundException.class })
+  public ResponseEntity<ApiErrorResponse> NotFoundException(NotFoundException e) {
+    ApiErrorResponse errorResponse = new ApiErrorResponse(e.getMessage(), HttpStatus.NOT_FOUND, ZonedDateTime.now());
+    return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler({ MethodArgumentNotValidException.class })

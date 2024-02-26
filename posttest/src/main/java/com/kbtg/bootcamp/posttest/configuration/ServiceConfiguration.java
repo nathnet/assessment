fddb3dc6c36113ec8d54f79ffa00121f8567eae9
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-26 04:09:31 -0800

    Instantiate new beans

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/configuration/ServiceConfiguration.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/configuration/ServiceConfiguration.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.kbtg.bootcamp.posttest.controller.AdminController;
+import com.kbtg.bootcamp.posttest.controller.LotteryController;
+import com.kbtg.bootcamp.posttest.controller.UserController;
 import com.kbtg.bootcamp.posttest.repository.LotteryRepository;
+import com.kbtg.bootcamp.posttest.repository.UserLotteryRepository;
+import com.kbtg.bootcamp.posttest.repository.UserRepository;
 import com.kbtg.bootcamp.posttest.service.LotteryService;
 
 
@@ -13,9 +18,30 @@ public class ServiceConfiguration {
 
   @Autowired
   LotteryRepository lotteryRepository;
+  
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  UserLotteryRepository userLotteryRepository;
+
+  @Bean
+  public AdminController adminController() {
+    return new AdminController(lotteryService());
+  }
+
+  @Bean
+  public LotteryController lotteryController() {
+    return new LotteryController(lotteryService());
+  }
+
+  @Bean
+  public UserController userController() {
+    return new UserController(lotteryService());
+  }
 
   @Bean
   public LotteryService lotteryService() {
-    return new LotteryService(lotteryRepository);
+    return new LotteryService(lotteryRepository, userRepository, userLotteryRepository);
   }
 }

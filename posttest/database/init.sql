3495aa85c5827fb43ae8451711dd613ca4de02e0
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2024-02-25 16:25:46 -0800

    Fixed initial db schema

--- posttest/database/init.sql
+++ posttest/database/init.sql
@@ -1,22 +1,25 @@
 DROP TABLE IF EXISTS lottery;
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS user_ticket;
 
 CREATE TABLE lottery (
   lottery_id char(6) PRIMARY KEY,
-  price numeric(, 2),
-  amount_available int,
+  price numeric(15, 2),
+  amount_available int
+);
+
+CREATE TABLE users (
+  user_id char(10) PRIMARY KEY,
+  name varchar(255),
+  email varchar(255),
+  password varchar(255)
 );
 
 CREATE TABLE user_ticket(
-  FOREIGN KEY (lottery_id) char(6) REFERENCES lottery (lottery_id),
-  FOREIGN KEY (user_id) varchar(255) REFERENCES user (user_id),
-  amount int,
-);
-
-CREATE TABLE user (
-  pk serial PRIMARY KEY,
-  user_id uuid NOT NULL,
-  name varchar(255),
-  email varchar(255),
-  password varchar(255),
+  transaction_id serial PRIMARY KEY,
+  lottery_id char(6) NOT NULL,
+  user_id char(10) NOT NULL,
+  ticket_amount int,
+  FOREIGN KEY (lottery_id) REFERENCES lottery (lottery_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
